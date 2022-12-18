2caf9ec9e744c86c6dee56ea6b172fbf0fcbcce1
Mohammad Mahdi Ghoreishi <mr.mmghoreishi@gmail.com>
2022-12-18 21:46:36 +0330

    I added Server-Side-Validation + Client-Side-Validation for Contact-Form to make it safer

--- pages/api/contacts/index.js
+++ pages/api/contacts/index.js
@@ -1,5 +1,5 @@
 import { connectDatabase, insertDocument } from "../../../helpers/db-util";
-import { validateEmptyForm } from "../../../helpers/validation";
+import { ValidateEmail, validateEmptyForm } from "../../../helpers/validation";
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
@@ -13,6 +13,18 @@ const handler = async (req, res) => {
 
     try {
       const { contactData } = req.body;
+
+      const validateEmpty = validateEmptyForm(contactData);
+      if (validateEmpty) {
+        res.status(500).json({ message: "All fields are required" });
+        return;
+      }
+
+      if (!ValidateEmail(contactData.email)) {
+        res.status(500).json({ message: "The email-format is wrong" });
+        return;
+      }
+
       await insertDocument(client, "contacts", contactData);
       client.close();
       res.status(201).json({ message: "It added the message well!" });
