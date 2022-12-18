cc06dbbb560ba389c1ffdb601a032ab471003805
Mohammad Mahdi Ghoreishi <mr.mmghoreishi@gmail.com>
2022-12-18 19:32:22 +0330

    Remove test
    Remove test

--- pages/index.jsx
+++ pages/index.jsx
@@ -34,7 +34,6 @@ const Home = () => {
 
   return (
     <>
-      <h1>TEST!</h1>
       <AddTitle title="صفحه اصلی" />
       <section id="hero" className="d-flex align-items-center">
         <div className="container">
