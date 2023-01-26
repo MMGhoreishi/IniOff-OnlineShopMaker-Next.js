6f652c60f9e436813817b75f3e92ec43aa41bcbf
Mohammad Mahdi Ghoreishi <mr.mmghoreishi@gmail.com>
2023-01-27 00:52:00 +0330

    I added pagination to the search page

--- pages/search/[category].jsx
+++ pages/search/[category].jsx
@@ -8,6 +8,7 @@ import {
   ModalForSearch,
   NothingFound,
   AddTitle,
+  Pagination,
 } from "../../components";
 
 const ACTIONS = {
@@ -135,33 +136,7 @@ const Category = ({ products, category, getShowPermission, statusNumber }) => {
               </div>
               <div className="row">
                 <div className="col">
-                  <ul className="pagination pagination-lg justify-content-center">
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        Previous
-                      </a>
-                    </li>
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        1
-                      </a>
-                    </li>
-                    <li className="page-item active">
-                      <a className="page-link" href="#">
-                        2
-                      </a>
-                    </li>
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        3
-                      </a>
-                    </li>
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        Next
-                      </a>
-                    </li>
-                  </ul>
+                  <Pagination />
                 </div>
               </div>
             </>
